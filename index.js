48e3f5e188640ce7d95d1ea3ea37d5e7119ddd2d
Nicolas Martinez V <nicolas.martinez@karibu.cl>
2019-04-16 07:07:29 -0400

    wip

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const http = require('http');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
